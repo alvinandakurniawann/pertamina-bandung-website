ef17f7a3fece39909be683f7c4d05d644b7bff1d
alvinandakurniawann <alvinanda2004@gmail.com>
2025-08-29 23:14:57 +0700

    Create page.tsx

---
+++ src/app/overview/page.tsx
@@ -0,0 +1,11 @@
+import IndexClient from '@/components/IndexClient'
+
+export const dynamic = 'force-dynamic'
+export const revalidate = 0
+export const fetchCache = 'force-no-store'
+
+export default function Page() {
+  return <IndexClient />
+}
+
+
